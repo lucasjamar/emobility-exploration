7171320d8eb78d086b5dd8ca0021df82ae61d5f2
Lucas Jamar <39372418+lucasjamar@users.noreply.github.com>
2022-09-16 18:20:03 +0200

    working dir

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import streamlit as st
 import streamlit.components.v1 as components
 from pivottablejs import pivot_ui
 import pandas as pd
+import os
 
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 
 st.set_page_config(layout="wide", page_title="E Mobility")
 st.title("E Mobility Exploration")
